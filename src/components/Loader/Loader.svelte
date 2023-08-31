6cd381a8161b46056c565ba419b92a97c05d3faf
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-08-31 22:41:36 +0200

    add Loader component for lazy loading

---
+++ src/components/Loader/Loader.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<style lang="scss">
+	.loader {
+		@apply w-16 h-16;
+		@apply border-solid border-8 border-gray-600
+			 border-t-indigo-400
+			   rounded-full;
+		animation: spin 1s linear infinite;
+	}
+	@keyframes spin {
+		0% { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
+	}
+	</style>
+	
+<div class="loader">
+</div>

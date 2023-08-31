5713259f9125aa5b11cdaf3891d0fb236aac6078
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-08-31 22:42:21 +0200

    add nojs component to show stuff when js is disabled

---
+++ src/components/nojs/Nojs.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+	let ref: HTMLDivElement
+
+	onMount(() => {
+		ref.remove()
+	})
+</script>
+
+<div
+	bind:this={ref}
+>
+	<slot/>
+</div>

20352d72aa9ba83202593e6868714192489b48b0
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-08-31 22:38:16 +0200

    add custom button component

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let className = '';
+	export { className as class }
+	export let style: string = ""
+	export let click: () => void = () => {}
+</script>
+
+<button
+	style={style}
+	class="flex items-center justify-center rounded p-4 p-2
+		   focus:translate-y-[0.075rem]
+		   hover:opacity-90 transition-all duration-75
+		   {className}
+	"
+	on:click={click}
+>
+	<slot/>
+</button>

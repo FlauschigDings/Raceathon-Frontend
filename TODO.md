a54fcf63ccb329d102138d82b9a6954babc740f7
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-08-31 22:37:55 +0200

    update init stuff

---
+++ TODO.md
@@ -0,0 +1,20 @@
+- edit account
+- notifications
+- QRCode
+	Show / Fullscreen
+	Refresh
+- Sponsor
+	Create Sponsoring
+	Delete Sponsoring
+
+	Sponsoren List
+	Group Sponsoring List
+	Generate Sponsor
+- Groups
+	Create
+	Delete
+	Private / Public
+
+- SponsorHub (Läufer / Sponsoren)
+	Search
+	Request
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import postcss from './postcss.config.js';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  css:{
-    postcss
-  }
-})
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			"@service": path.resolve("src/service"),
+			"@routes": path.resolve("src/routes"),
+			"@__tests__": path.resolve("src/__tests__"),
+			"@components": path.resolve("src/components"),
+		}
+	},
+	server: {
+		proxy: {
+			'/api': {
+				changeOrigin: true,
+				target: 'http://127.0.0.1:8000',
+			},
+		}
+	}
+});

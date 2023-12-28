42d0366888cc5636cdd7032c7d809406e607e44b
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-12-28 02:13:16 +0100

    add custom card with scroll

--- src/components/Card/Card.svelte
+++ src/components/Card/Card.svelte
@@ -1,6 +1,27 @@
-<div class="shadow rounded-md bg-indigo-400/20 p-3 mb-2 flex-1">
-	<h3 class="text-2xl">
-		<slot name="head"/>
-	</h3>
-	<slot name="body" />
+<script lang="ts">
+	import waves from "@image/layered-waves-haikei.svg";
+
+	let className = "";
+	export { className as class }
+</script>
+
+<div class="flex flex-col bg-slate-700 h-full relative overflow-hidden rounded-2xl z-10 shadow {className}">
+	<div class="
+		w-full h-full
+		absolute -z-10
+		opacity-10 bg-center
+		bg-cover
+		rotate-180
+		grayscale
+		rounded-2xl"
+		style="background-image: url({waves})"
+	/>
+	<div class="p-4 flex flex-col h-full">
+		<h4 class="text-xl">
+			<slot name="title"/>
+		</h4>
+		<div class="z-30 overflow-auto h-full">
+			<slot/>
+		</div>
+	</div>
 </div>

43467fe894b7b84f698b9df631462839b85f578f
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-12-28 02:12:39 +0100

    custom List element

---
+++ src/components/list/default/Item.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+	let className: string = ""
+	export { className as class }
+
+</script>
+
+<li>
+	<div class={className}>
+		<slot/>
+	</div>
+	<div class="rounded-full h-0.5 bg-slate-400/20"></div>
+</li>
---
+++ src/components/list/default/List.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+	let className: string = ""
+	export { className as class }
+
+</script>
+
+
+<ul class={className}>
+	<slot/>
+</ul>

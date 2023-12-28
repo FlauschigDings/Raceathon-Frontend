173cdcc1e7f87f26d2abb8a39d1f5c6a3ea42b3f
Eiswuerfel13 <erikmue13@gmail.com>
2023-12-28 03:27:22 +0100

    Add CopyButton Component

---
+++ src/components/Button/CopyButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import BsClipboard2 from "svelte-icons-pack/bs/BsClipboard2";
+	import BsClipboard2Check from "svelte-icons-pack/bs/BsClipboard2Check";
+
+	import Button from "./Button.svelte";
+	import { ButtonDesign } from "./button";
+
+	export let name1: string = "Kopieren"
+	export let name2: string = "Kopiert"
+
+	export let clipText: string
+
+	$: state = true
+	$: stateText = (state)? name1 : name2;
+
+	function copy(str: string) {
+		state = false
+		copyToClipboard(str)
+		setTimeout(() => {
+			state = true
+		}, 2e3)
+	}
+	
+	function copyToClipboard(str: string) {
+		navigator.clipboard.writeText(str);
+	}
+</script>
+
+<Button design={ButtonDesign.SECONDARY} click={() => copy(clipText)}>
+	<Icon className="fill-white mr-1" src={(state)? BsClipboard2 : BsClipboard2Check}/>{stateText}
+</Button>
---
+++ src/stories/CopyButton.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+	import CopyButton from "@components/Button/CopyButton.svelte";
+	import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Meta title="CopyButton" component={CopyButton}/>
+
+<Template let:args>
+	<CopyButton clipText={args.clip}/>
+</Template>
+
+<Story name="CopyButton" args={{clip: "Hallo ich bin Clipy dein Clipboard"}}/>

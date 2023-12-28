ea630210e682d52f6adf184620df3590bddf60c9
FlauschigDings <nicky.aka.nightnicky@gmail.com>
2023-12-28 02:11:42 +0100

    add custom badges design

---
+++ src/components/Badge/Badge.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Badges } from "./badge";
+
+	export let badge: Badges = Badges.SUCCESS
+</script>
+
+<span class="text-xs rounded-md px-2 py-0.5 border {badge}">
+	<slot/>
+</span>
---
+++ src/components/Badge/badge.ts
@@ -0,0 +1,10 @@
+export enum Badges {
+	PRIMARY   = "border-primary-500/30 bg-primary-500/10 text-primary-500",
+	SECONDARY = "border-secondary-500/30 bg-secondary-500/10 text-secondary-500",
+
+	SUCCESS = "border-success-500/30 bg-success-500/10 text-success-500",
+	WARNING = "border-warning-500/30 bg-warning-500/10 text-warning-500",
+	DANGER  = "border-danger-500/30 bg-danger-500/10 text-danger-500",
+	INFO    = "border-info-500/30 bg-info-500/10 text-info-500",
+	PENDING = "border-pending-400/30 bg-pending-400/10 text-pending-400",
+}

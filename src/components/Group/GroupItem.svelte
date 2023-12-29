1155c6a7a2e19c595824a0a8ddc71a31bad495b6
Eiswuerfel13 <erikmue13@gmail.com>
2023-12-29 17:46:18 +0100

    Added a Group Comonnent 🚧

---
+++ src/components/Group/GroupItem.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import AccordionItem from '@components/Accordion/AccordionItem.svelte';
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import FaSolidMoneyBillWaveAlt from "svelte-icons-pack/fa/FaSolidMoneyBillWaveAlt";
+	import AiOutlineSolution from "svelte-icons-pack/ai/AiOutlineSolution";
+
+	export let groupName: string
+	export let fixAmountPamyment: number
+	export let teilnehmerNamen: string[] | null = [];
+
+
+	function openGroupOptions(){
+		alert("test")
+	}
+</script>
+
+<!-- Outer Div -->
+<AccordionItem id={groupName}>
+	<span slot="title" class="flex flex-auto p-2 flex-wrap flex-row">
+		<p class="basis-1/2 text-left text-sm">
+			<Icon className="fill-white inline mr-1" src={AiOutlineSolution} />Groupname
+		</p>
+		<p class="basis-1/2 text-right text-sm">
+			<Icon className="fill-white inline mr-1" src={FaSolidMoneyBillWaveAlt} />pay / Person
+		</p>
+		<p class="basis-1/2 text-left">{groupName}</p>
+		<p class="basis-1/2 text-right inline">{fixAmountPamyment.toLocaleString('de-DE', { minimumFractionDigits: 2 })}€</p>
+	</span>
+	{#if teilnehmerNamen != null}
+		{#each teilnehmerNamen as teilnehmerName}
+			<p class="block">{teilnehmerName}</p>
+		{/each}
+	{/if}
+</AccordionItem>
---
+++ src/stories/GroupItem.stories.svelte
@@ -0,0 +1,15 @@
+<script>
+    import GroupItem from "@components/Group/GroupItem.svelte";
+	import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+    import Accordion from "@components/Accordion/Accordion.svelte";
+</script>
+
+<Meta title="Group" component={GroupItem}/>
+
+<Template let:args>
+	<Accordion>
+		<GroupItem groupName={args.groupName} fixAmountPamyment={args.fixAmountPamyment} teilnehmerNamen={args.teilnehmer}/>
+	</Accordion>
+</Template>
+
+<Story name="Group" args={{groupName: "Renner AG",fixAmountPamyment: 100,teilnehmer: ["Test1","Test2","Test3","Test4","Test5","Test6"]}}/>

8a99b20bfea61458022200c9b12f68bf5a4540cb
Eiswuerfel13 <erikmue13@gmail.com>
2024-01-01 19:05:20 +0100

    refactored the GroupItem Component ♻️🚧

--- src/components/Group/GroupItem.svelte
+++ src/components/Group/GroupItem.svelte
@@ -25,33 +25,36 @@
 
 <!-- Outer Div -->
 <AccordionItem id={groupName}>
-	<span slot="title" class="w-full">
+	<span slot="title" class="w-full px-3 py-2">
 
-		<div class="grid grid-cols-10 grid-rows-2">
+		<div class="flex flex-auto flex-wrap flex-row justify-between">
 
-			<p class="col-span-3 text-left text-sm">
-				<Icon className="fill-white inline mr-1" src={AiOutlineSolution} />Groupname
-			</p>
-
-			<p class="col-span-3 text-center text-sm">
-				<Icon className="fill-white inline mr-1" src={FaSolidMoneyBillWaveAlt} />pay / Person
-			</p>
-
-			<Button class="col-span-3 row-span-2 h-3/5 w-4/5 m-auto z-100"
-			design={ButtonDesign.PRIMARY} on:click= {() => {openGroupOptions()}}>
-			<Icon className="fill-white inline mr-1" src={AiOutlineUserAdd}></Icon>	Invite
-			</Button>
-
-			<div class=""><Icon className="fill-white inline" src={BsThreeDotsVertical}></Icon></div>
-		
-
-			<p class="col-span-3 text-left">{groupName}</p>
-			<p class="col-span-3 text-center inline">{fixAmountPamyment.toLocaleString('de-DE', { minimumFractionDigits: 2 })}€</p>
+			<!-- GroupName -->
+			<div class="basis-1/3">
+				<p class=" text-left text-sm text-slate-300/60">
+					<Icon className="fill-slate-300/60 inline mr-1" src={AiOutlineSolution} />Groupname
+				</p>
+				<p class="text-left">{groupName}</p>
+			</div>
+			<!-- pay / Person -->
+			<div class="text-left">
+				<p class="text-sm text-slate-300/60">
+					<Icon className="fill-slate-300/60 inline mr-1" src={FaSolidMoneyBillWaveAlt} />pay / Person
+				</p>
+				<p class="inline">{fixAmountPamyment.toLocaleString('de-DE', { minimumFractionDigits: 2 })}€</p>
+			</div>
+			<!-- Menu -->
+			<div on:click={() => {openGroupOptions()}} class="row-span-2 grid items-center place-items-center"><Icon className="fill-white inline" src={BsThreeDotsVertical}></Icon></div>
 		</div>
 	</span>
 	{#if teilnehmerNamen != null}
 		{#each teilnehmerNamen as teilnehmerName,i}
-		{#if i == 0}<span class="h-5 block"></span>{/if}
+		{#if i == 0}
+			<Button class="row-span-2 h-10 w-3/5 m-auto mt-2 mb-2"
+			design={ButtonDesign.PRIMARY} on:click= {() => {openGroupOptions()}}>
+			<Icon className="fill-white inline mr-1" src={AiOutlineUserAdd}></Icon>	Invite
+			</Button>
+		{/if}
 			<div class="grid grid-cols-2">
 				<p class="text-left">{teilnehmerName}</p>
 				<p class="content-end flex flex-auto flex-wrap flex-row-reverse">
@@ -63,5 +66,3 @@
 		{/each}
 	{/if}
 </AccordionItem>
-
-<style></style>
